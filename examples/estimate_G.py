91b8f2d14c866703b00202d4c1ed0fa49534ec5b
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-06-24 21:35:01 -0700

    Shear calculation scripts

---
+++ examples/estimate_G.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env  python
+
+import sys
+import numpy as np
+from scipy import stats
+
+
+def G_func(x, G, c):
+    return G*x + c
+
+
+def contact_indicies(data_contcs):
+    X0 = data_contcs[0]
+    N = len(data_contcs)
+    for i in range(N):
+        if data_contcs[i] < X0:
+            return i
+
+    return N
+
+
+def get_file(fname):
+    data = np.loadtxt(fname)
+    data = data.T
+
+
+    strain = data[0]
+    stress = data[2]
+    contacts = data[4]
+    
+    last_idx = contact_indicies(contacts)
+    strain = strain[:last_idx]
+    stress = stress[:last_idx]
+
+    return strain, stress
+
+
+def fit(strain_, stress_):
+    if len(strain) < 3:
+        return -1,-1
+
+    slope, intercept, r_value, p_value, std_err = stats.linregress(strain_, stress_)
+    
+    return slope, intercept
+
+fin = sys.argv[1]
+
+dphi = float(sys.argv[2])
+P0 = float(sys.argv[3])
+
+strain, stress = get_file(fin)
+G_, c_ = fit(strain, stress)
+
+foutname = 'GFIT_' + fin
+fout = open(foutname, 'w')
+fout.write(str(dphi) + " " + str(P0) + " "+ str(G_) + " " + str(c_) + "\n")
+fout.close()
+
+
---
+++ examples/run_shear.sh
@@ -0,0 +1,79 @@
+#! /bin/bash -x
+
+MY_PATH=`pwd`
+run_dir=$MY_PATH"/shear_dir"
+exe_file=$MY_PATH"/../shear_yeast_linearshear"
+cd $run_dir
+
+att=0.0      # no attractions
+cell="BUDD"  # type of the cell
+celltype=2
+rate0=0.002  # growth rate (in units of time-step)
+skip=25      # save frame every $skip steps
+AP=2.0       # NUMBER NOT USED
+desync=0.4   # desynchronize growth rate
+distrem=4.5  # NUMBER NOT USED
+ar=1.01      # BUD OF THE SIZE 1% OF THE INITIAL SIZE
+steps=10000  # NOT USED
+divtype=4
+
+INPUT_LIST="RUN_LIST_Lx15_51_to_100.txt"
+
+INPUT_LINE=`head -n $SLURM_ARRAY_TASK_ID $INPUT_LIST | tail -n 1`
+
+P0=0.001
+Lx=10
+mm=1
+nn=3
+se=123
+
+
+seed=$(($se+1000))
+SEED2=$(( -1 * $seed)) # make seed negative
+
+Ly=$Lx
+dphi=$mm"e-"$nn
+
+
+if [ ${P0%.*} -eq -1 ]
+then
+suffix=${cell}_ar${ar}_div_${divtype}_desync${desync}_seed_$seed"_"Lx${Lx}_Ly${Ly}_a${AP}_att${att}.dat
+else
+suffix=${cell}_ar${ar}_div_${divtype}_desync${desync}_seed_$seed"_"Lx${Lx}_Ly${Ly}_a${AP}_att${att}_P${P0}.dat
+fi
+
+
+file_="LF_DPHI_prod_"$suffix
+
+
+#### SPRAWDZ CZY PLIK JEST DLUZSZY NIZ 1 LINIA
+cp $MY_PATH"/output"/$file_  .
+
+
+if [[ $(wc -l <$file_) -ge 2 ]] ; then
+
+time $exe_file <<EOF
+   $ar
+   $Lx
+   $Ly
+   $AP
+   $celltype
+   $divtype
+   $P0
+   $att
+   $rate0
+   $desync
+   $steps
+   $SEED2
+   $distrem
+   $skip
+   $dphi
+   $file_
+   1e-6
+   5000
+EOF
+
+python $MY_PATH"./estimate_G.py" "G_data_"$file_ $dphi $P0
+
+fi   
+
