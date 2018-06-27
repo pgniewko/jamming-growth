b253868d5109b8b9c2c983bbea482ad8264c4027
Pawel Gniewek <gniewko.pablo@gmail.com>
2018-06-26 20:57:26 -0700

    ran2 function removed

--- src/shear_yeast_linearshear.f
+++ src/shear_yeast_linearshear.f
@@ -17,7 +17,7 @@
       parameter(Ntot=4096,Ngen=4096)
       double precision pi
       parameter(pi=3.1415926535897932d0)
-      double precision x(Ntot),y(Ntot),th(Ntot),D(Ntot),D1,exp,ran2
+      double precision x(Ntot),y(Ntot),th(Ntot),D(Ntot),D1,exp
       double precision x00(Ntot),y00(Ntot)
       double precision ftol,ftol1,fret,width,Lx,Ly,ratei
       double precision alpha(Ntot),rate(Ntot),alphar(Ntot),scale(Ntot)
@@ -38,26 +38,11 @@
       character file_prod*150
       character file_G*150
       
-      double precision xcc(Ntot),ycc(Ntot),thcc(Ntot),Dcc(Ntot)
-      double precision alphacc(Ntot),ratecc(Ntot),alpharcc(Ntot)
-      double precision scalecc(Ntot)
-      double precision Pcc,PPcc(Ntot),D0cc(Ntot)
-      double precision aclonecc(Ntot*Ngen)
-      double precision tbirthcc(Ntot*Ngen)
-      double precision xbirthcc(Ntot*Ngen),ybirthcc(Ntot*Ngen)
-      double precision xdivcc(999,2),ydivcc(999,2),thdivcc(999,2)
-      integer Ncc,ccc(Ntot,Ngen),Nexistcc
-      integer nclonecc(Ntot*Ngen),ncloneboxcc(Ntot*Ngen)
-      integer agecc(Ntot),agehistcc(Ngen,2)
-      integer idivcc(999,2), ndivcc
-      integer F(Ntot), Nc, Ziso, F_e(Ntot)
-      integer BUDCONT(Ntot), BUDCONTCC(Ntot), ZEROBUDS
-      double precision phitemp, calc_phi, wide
-      integer N_j,Ziso_j,Nc_j,Nf_j,Nu_j,Nmm_j,Nbb_j,Nmb_j
-      double precision phi_j,phi_orig,ddelrx,delrx
+      integer F(Ntot), Nc, Ziso
+      double precision calc_phi, wide
+      double precision phi_init,ddelrx,delrx
       
       integer ddelrx_steps, delrx_index
-      
       double precision SSTRESS, cory, SSTRESS0
 
 ! END OF NEW DATA      
@@ -91,7 +76,6 @@
       ftol1=0.5d-16  ! Condition 2 for frprmn: dV/N < ftol1
       rat=1.5d0      ! ratio of initial cell 2 to cell 1 volume
       
-      phitemp = 0.0d0
       wide = 2.0d0
 
       SSTRESS = 0d0
@@ -110,9 +94,9 @@
       write(*,*)file_prod
       write(*,*)file_G
      
-      phi_orig = 0d0
+      phi_init = 0d0
       
-      read(1, *) N, phi_orig
+      read(1, *) N, phi_init
       do i=1,N
           read(1, *) x(i),y(i),D(i),alpha(i),th(i)
       enddo
@@ -1106,41 +1090,4 @@ c  (C) Copr. 1986-92 Numerical Recipes Software Dt+;39.
 
       return
       END
-C  (C) Copr. 1986-92 Numerical Recipes Software Dt+;39.
-
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      FUNCTION ran2(idum)
-      INTEGER idum,IM1,IM2,IMM1,IA1,IA2,IQ1,IQ2,IR1,IR2,NTAB,NDIV
-      double precision ran2,AM,EPS,RNMX
-      PARAMETER (IM1=2147483563,IM2=2147483399,AM=1./IM1,IMM1=IM1-1,
-     *IA1=40014,IA2=40692,IQ1=53668,IQ2=52774,IR1=12211,IR2=3791,
-     *NTAB=32,NDIV=1+IMM1/NTAB,EPS=1.2e-7,RNMX=1.-EPS)
-      INTEGER idum2,j,k,iv(NTAB),iy
-      SAVE iv,iy,idum2
-      DATA idum2/123456789/, iv/NTAB*0/, iy/0/
-      if (idum.le.0) then
-        idum=max(-idum,1)
-        idum2=idum
-        do 11 j=NTAB+8,1,-1
-          k=idum/IQ1
-          idum=IA1*(idum-k*IQ1)-k*IR1
-          if (idum.lt.0) idum=idum+IM1
-          if (j.le.NTAB) iv(j)=idum
-11      continue
-        iy=iv(1)
-      endif
-      k=idum/IQ1
-      idum=IA1*(idum-k*IQ1)-k*IR1
-      if (idum.lt.0) idum=idum+IM1
-      k=idum2/IQ2
-      idum2=IA2*(idum2-k*IQ2)-k*IR2
-      if (idum2.lt.0) idum2=idum2+IM2
-      j=1+iy/NDIV
-      iy=iv(j)-idum2
-      iv(j)=idum
-      if(iy.lt.1)iy=iy+IMM1
-      ran2=min(AM*iy,RNMX)
-      return
-      END
-C  (C) Copr. 1986-92 Numerical Recipes Software .              
+C  (C) Copr. 1986-92 Numerical Recipes Software Dt+;39.              
